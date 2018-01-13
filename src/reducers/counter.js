e1c7e7643bca2a917cc8ced32367b51e0ffd67f0
Saurabh S <saurabh@appler.xyz>
2018-01-13 20:29:02 +0530

    more jamming ... made the counter work .. yeah !!!

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -4,11 +4,11 @@ const counter = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
     	return Object.assign({}, state, {
-        count: (state.count+1)
+        count: (state.count + 1)
       });
     case 'DECREMENT':
     	return Object.assign({}, state, {
-        count: (state.count-1)
+        count: (state.count - 1)
       });
     default:
       return state;
